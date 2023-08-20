22e4feb3422c6c7238e6fd397f26393b130fc7c8
Ran Shaashua <ranshaa05@gmail.com>
2023-08-20 17:23:01 +0300

    black formatting

--- gui.py
+++ gui.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
-from tkinter import filedialog #tkinter bug
+from tkinter import filedialog  # tkinter bug
 from tkinter import IntVar
 from os import path
 
@@ -8,7 +8,7 @@ from os import path
 import converter
 
 
-class Gui():
+class Gui:
     def __init__(self, root=None):
         if not root:
             self.root = tk.Tk()
@@ -30,40 +30,67 @@ class Gui():
 
         self.progress = IntVar()
 
-
     def check_params(self, src_path, dst_path):
-        if src_path == "" or dst_path == "\\":  #check if paths are usable
-            tk.messagebox.showerror("Error", "You must enter a source and destination path.")
+        if src_path == "" or dst_path == "\\":  # check if paths are usable
+            tk.messagebox.showerror(
+                "Error", "You must enter a source and destination path."
+            )
         elif src_path == dst_path:
-            tk.messagebox.showerror("Error", "Source and destination folders cannot be the same.")
+            tk.messagebox.showerror(
+                "Error", "Source and destination folders cannot be the same."
+            )
         elif not path.exists(src_path):
-            tk.messagebox.showerror("Error", "Source folder does not exist.\nPlease select a different folder.")
+            tk.messagebox.showerror(
+                "Error",
+                "Source folder does not exist.\nPlease select a different folder.",
+            )
         elif not path.exists(dst_path):
-            tk.messagebox.showerror("Error", "Destination folder does not exist.\nPlease select a different folder.")
-        elif path.isdir(dst_path + path.basename(src_path)):    #TODO: maybe add an option to override current folder?
-            tk.messagebox.showerror("Error", f'Destination folder already has a folder named "{path.basename(src_path)}" in it.\nPlease select a different destination folder.')
+            tk.messagebox.showerror(
+                "Error",
+                "Destination folder does not exist.\nPlease select a different folder.",
+            )
+        elif path.isdir(
+            dst_path + path.basename(src_path)
+        ):  # TODO: maybe add an option to override current folder?
+            tk.messagebox.showerror(
+                "Error",
+                f'Destination folder already has a folder named "{path.basename(src_path)}" in it.\nPlease select a different destination folder.',
+            )
 
         else:
             return src_path, dst_path
 
     def browse(self, path_field, field_num):
-       path = (tk.filedialog.askdirectory(mustexist=True, title="Select Destination Folder").replace("/", "\\"))
-       path_field[field_num].delete(0, tk.END)
-       path_field[field_num].insert(0, path)
+        path = tk.filedialog.askdirectory(
+            mustexist=True, title="Select Destination Folder"
+        ).replace("/", "\\")
+        path_field[field_num].delete(0, tk.END)
+        path_field[field_num].insert(0, path)
 
     def build_window(self):
         self.header = tk.Label(self.root, text="Enter path:")
         self.box1_text = tk.Label(self.root, text="Source folder:")
         self.box2_text = tk.Label(self.root, text="Destination folder:")
-        self.start_button = tk.Button(self.root, text="Start", command=lambda: converter.AppLogic.convert(self))
+        self.start_button = tk.Button(
+            self.root, text="Start", command=lambda: converter.AppLogic.convert(self)
+        )
         self.box3_text = tk.Label(self.root, text="Quality:")
         self.quality_dropdown = ttk.Combobox(self.root, state="readonly", width=15)
-        self.quality_dropdown["values"] = ("Lossless", *[str(i) for i in range(100, -1, -1)])
+        self.quality_dropdown["values"] = (
+            "Lossless",
+            *[str(i) for i in range(100, -1, -1)],
+        )
         self.quality_dropdown.current(0)
-        #TODO: find a way to add a "%" to the end of progressbar_text"
+        # TODO: find a way to add a "%" to the end of progressbar_text"
         self.progressbar_text = tk.Label(self.root, textvariable=self.progress)
 
-        self.progressbar = ttk.Progressbar(self.root, variable=self.progress, orient="horizontal", length=300, mode="determinate")
+        self.progressbar = ttk.Progressbar(
+            self.root,
+            variable=self.progress,
+            orient="horizontal",
+            length=300,
+            mode="determinate",
+        )
 
         self.header.grid(row=0, column=2, columnspan=1)
         self.box1_text.grid(row=3, column=1, sticky="w")
@@ -73,20 +100,21 @@ class Gui():
         self.start_button.grid(row=6, column=2)
         self.progressbar_text.grid(row=7, column=2)
         self.progressbar.grid(row=8, column=2)
-        
 
         self.fields = []
         for i in range(2):
             path_field = tk.Entry(self.root, width=50)
-            path_field.grid(row=i+3, column=2)
-            browse_button = tk.Button(self.root, text="Browse...", command=lambda field_num=i: self.browse(self.fields, field_num))
-            browse_button.grid(row=i+3, column=3)
+            path_field.grid(row=i + 3, column=2)
+            browse_button = tk.Button(
+                self.root,
+                text="Browse...",
+                command=lambda field_num=i: self.browse(self.fields, field_num),
+            )
+            browse_button.grid(row=i + 3, column=3)
             self.fields.append(path_field)
-        
 
     def update_progressbar(self):
         self.progressbar.update()
-
 
     def post_conversion_dialogue(self, num_of_images, num_of_non_images):
         if num_of_images == 0:
@@ -94,17 +122,20 @@ class Gui():
             return False
         elif num_of_non_images > 0:
             return tk.messagebox.askyesno(
-"Copy non-images?",
-f"""{num_of_images} files were successfully converted.
+                "Copy non-images?",
+                f"""{num_of_images} files were successfully converted.
 {num_of_non_images} files could not be converted. they are most likely not images.
-Copy non-images to destination folder?"""
-)
+Copy non-images to destination folder?""",
+            )
         else:
-            tk.messagebox.showinfo("Conversion complete", f"{num_of_images} files were successfully converted.")
+            tk.messagebox.showinfo(
+                "Conversion complete",
+                f"{num_of_images} files were successfully converted.",
+            )
             return False
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     gui = Gui()
     gui.build_window()
     gui.root.mainloop()
-    
