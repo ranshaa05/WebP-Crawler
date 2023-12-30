ba0f05b6077b4fcf2baebd031aa51c81a209fa8c
Ran Shaashua <47498956+ranshaa05@users.noreply.github.com>
2023-12-31 00:27:08 +0200

    switched to Customtkinter + CTkMessageBox

--- converter.py
+++ converter.py
@@ -1,9 +1,14 @@
 import os
+import logging
+import coloredlogs
+
 from pathlib import Path
 from shutil import copytree, ignore_patterns, copyfile
 from PIL import Image, UnidentifiedImageError
 
-import gui
+# logger setup
+log = logging.getLogger(__name__)
+coloredlogs.install(level="DEBUG", logger=log)
 
 
 class AppLogic:
@@ -19,7 +24,7 @@ class AppLogic:
         try:
             copytree(
                 src_path,
-                dst_path + "\\" + os.path.basename(os.path.normpath(src_path)),
+                os.path.join(dst_path, os.path.basename(os.path.normpath(src_path))),
                 symlinks=False,
                 ignore=ignore_files,
             )
@@ -27,69 +32,90 @@ class AppLogic:
             return False
 
     def convert(self):
-        src_path = self.fields[0].get().rstrip(" ")
-        dst_path = self.fields[1].get().rstrip(" ")
+        src_path = self.fields[0].get().strip()
+        dst_path = self.fields[1].get().strip()
         quality = self.quality_dropdown.get()
         if self.check_params(src_path, dst_path):
             AppLogic().create_folder_tree(src_path, dst_path)
             pass
-        else:
-            raise ValueError("Invalid parameters.")
 
-        file_list = list(Path(src_path).rglob("*.*"))
-        last_iter_length = 0  # TODO: make these class variables
-        num_of_image_files = 0
-        num_of_non_image_files = 0
-        total_num_of_files = 0
-        non_image_files = []
-        for i in file_list:
-            new_dst_path = (
-                dst_path + "\\" + os.path.basename(src_path) + str(i).split(src_path)[1]
-            )  # destination to original tree path
-            print(
-                f"Converting {os.path.basename(new_dst_path)}...",
-                end=" " * (last_iter_length - len(os.path.basename(new_dst_path)))
-                + "\r",
-            )
-            last_iter_length = len(os.path.basename(new_dst_path))
+            file_list = list(Path(src_path).rglob("*.*"))
+            file_list_length = len(file_list)
+            last_iter_length = 0  # TODO: make these class variables
+            num_of_image_files = 0
+            num_of_non_image_files = 0
+            num_of_skipped_files = 0
+            non_image_files = []
+            are_you_sure = False
+            for file in file_list:
+                new_dst_path = f"{dst_path}\\{os.path.basename(src_path) + str(file).split(src_path)[1]}"  # destination to original tree path
 
-            try:
-                image = Image.open(i)
-                if quality == "Lossless":
-                    image.save(
-                        new_dst_path[: new_dst_path.rfind(".")] + ".webp",
-                        format="webp",
-                        lossless=True,
-                    )
-                else:
-                    image.save(
-                        new_dst_path[: new_dst_path.rfind(".")] + ".webp",
-                        format="webp",
-                        quality=int(quality),
-                    )
+                print(
+                    f"Converting {os.path.basename(new_dst_path)}...",
+                    end=" " * (last_iter_length - len(os.path.basename(new_dst_path)))
+                    + "\r",
+                )
+                last_iter_length = len(os.path.basename(new_dst_path))
 
-                num_of_image_files += 1
-            except UnidentifiedImageError:
-                num_of_non_image_files += 1
-                non_image_files.append(i)
+                try:
+                    image = Image.open(file)
+                    num_of_image_files += 1
+                except UnidentifiedImageError:
+                    num_of_non_image_files += 1
+                    non_image_files.append(file)
 
-            total_num_of_files += 1
-            self.progress.set(
-                f"{int((total_num_of_files / len(list(file_list))) * 100)}%"
-            )
-            self.display_progress.set(str(self.progress.get()).strip("%"))
-            self.update_progressbar()
+                if image:
+                    if not self.show_overwrite_dialogues(new_dst_path, are_you_sure):
+                        num_of_skipped_files += 1
+                        image.close()
+                        self.update_progressbar(
+                            num_of_image_files,
+                            num_of_non_image_files,
+                            num_of_skipped_files,
+                            file_list_length,
+                        )
+                        continue  # continue if user chooses to skip overwriting this file.
 
-        if self.post_conversion_dialogue(num_of_image_files, num_of_non_image_files):
-            for i in non_image_files:
-                copyfile(
-                    i,
-                    dst_path
-                    + "\\"
-                    + os.path.basename(src_path)
-                    + str(str(i).split(src_path)[1]),
+                    if quality == "Lossless":
+                        image.save(
+                            new_dst_path[: new_dst_path.rfind(".")] + ".webp",
+                            format="webp",
+                            lossless=True,
+                        )
+                    else:
+                        image.save(
+                            new_dst_path[: new_dst_path.rfind(".")] + ".webp",
+                            format="webp",
+                            quality=int(quality),
+                        )
+                    image.close()
+                    image = None
+
+                self.update_progressbar(
+                    num_of_image_files,
+                    num_of_non_image_files,
+                    num_of_skipped_files,
+                    file_list_length,
                 )
 
-        # Reset progress bar
-        self.progress.set("0%")
-        self.display_progress.set("0")
+            if self.post_conversion_dialogue(
+                num_of_image_files, num_of_non_image_files
+            ):
+                for file in non_image_files:
+                    copyfile(
+                        file,
+                        os.path.join(
+                            dst_path,
+                            os.path.basename(src_path),
+                            str(str(file).split(src_path)[1]),
+                        ),
+                    )
+
+            # Reset progress bar
+            self.progress.set("0%")
+            self.display_progress.set("0")
+
+            self.overwrite_all = False  # reset overwrite all flag
+
+        else:
+            log.warn("Invalid parameters. No changes have been made.")
--- gui.py
+++ gui.py
@@ -1,21 +1,25 @@
-import tkinter as tk
-from tkinter import ttk
-from tkinter import filedialog  # tkinter bug
+import customtkinter as ctk
+import os
+
+from CTkMessagebox import CTkMessagebox
 from tkinter import IntVar, StringVar
-from os import path
 
 
 import converter
 
-
 class Gui:
     def __init__(self, root=None):
         if not root:
-            self.root = tk.Tk()
+            self.root = ctk.CTk()
         else:
             self.root = root
+
         self.root.title("All To webp")
-        self.root.geometry("480x200")
+        screen_width = self.root.winfo_screenwidth()
+        screen_height = self.root.winfo_screenheight()
+        window_size = f"{int(screen_width * 0.32)}x{int(screen_height * 0.2)}"
+        self.root.geometry(window_size)
+        print(window_size)
 
         self.header = None
         self.box1_text = None
@@ -34,109 +38,224 @@ class Gui:
             IntVar()
         )  # hacky way to include the "%" sign in the progressbar text
 
+        self.overwrite_all = False
+        self.show_overwrite_all_dialogue = True
+
     def check_params(self, src_path, dst_path):
         if src_path == "" or dst_path == "\\":  # check if paths are usable
-            tk.messagebox.showerror(
-                "Error", "You must enter a source and destination path."
-            )
+            CTkMessagebox(title="Error", message="You must enter a source and destination path.", icon="cancel")
         elif src_path == dst_path:
-            tk.messagebox.showerror(
-                "Error", "Source and destination folders cannot be the same."
+            CTkMessagebox(
+                title="Error",
+                message="Source and destination folders cannot be the same.",
+                icon="cancel",
             )
-        elif not path.exists(src_path):
-            tk.messagebox.showerror(
-                "Error",
-                "Source folder does not exist.\nPlease select a different folder.",
+        elif not os.path.exists(src_path):
+            CTkMessagebox(
+                title="Error",
+                message="Source folder does not exist.\nPlease select a different folder.",
+                icon="cancel",
             )
-        elif not path.exists(dst_path):
-            tk.messagebox.showerror(
-                "Error",
-                "Destination folder does not exist.\nPlease select a different folder.",
+        elif not os.path.exists(dst_path):
+            create_destination_folder = CTkMessagebox(
+                title="Destination folder does not exist",
+                message="Destination folder does not exist.\nWould you like to create it?",
+                icon="warning",
             )
-        elif path.isdir(
-            dst_path + path.basename(src_path)
-        ):  # TODO: maybe add an option to override current folder?
-            tk.messagebox.showerror(
-                "Error",
-                f'Destination folder already has a folder named "{path.basename(src_path)}" in it.\nPlease select a different destination folder.',
-            )
+            if create_destination_folder:
+                os.mkdir(dst_path)
+                return src_path, dst_path
+            else:
+                return False
+        elif os.path.isdir(os.path.join(dst_path, os.path.basename(src_path))):
+            use_folder = CTkMessagebox(title="Destination folder already exists",
+                                message=f"Destination folder already has a folder named {os.path.basename(src_path)} in it.\nWould you like to use it anyway?",
+                                icon="question",
+                                option_1="Cancel",
+                                option_2="No",
+                                option_3="Yes")
+            if use_folder.get() == "Yes":
+                return src_path, dst_path
+
+            else:
+                CTkMessagebox(
+                    title="Select a different  folder.",
+                    message="Please select a different destination folder.",
+                    icon="warning",
+                )
+                return False
 
         else:
             return src_path, dst_path
 
     def browse(self, path_field, field_num):
-        path = tk.filedialog.askdirectory(
+        path = ctk.filedialog.askdirectory(
             mustexist=True, title="Select Destination Folder"
         ).replace("/", "\\")
-        path_field[field_num].delete(0, tk.END)
+        path_field[field_num].delete(0, ctk.END)
         path_field[field_num].insert(0, path)
 
     def build_window(self):
-        self.header = tk.Label(self.root, text="Enter path:")
-        self.box1_text = tk.Label(self.root, text="Source folder:")
-        self.box2_text = tk.Label(self.root, text="Destination folder:")
-        self.start_button = tk.Button(
+        self.header = ctk.CTkLabel(self.root, text="Enter path:")
+        self.box1_text = ctk.CTkLabel(self.root, text="Source folder:")
+        self.box2_text = ctk.CTkLabel(self.root, text="Destination folder:")
+        self.start_button = ctk.CTkButton(
             self.root, text="Start", command=lambda: converter.AppLogic.convert(self)
         )
-        self.box3_text = tk.Label(self.root, text="Quality:")
-        self.quality_dropdown = ttk.Combobox(self.root, state="readonly", width=15)
-        self.quality_dropdown["values"] = (
-            "Lossless",
-            *[str(i) for i in range(100, -1, -1)],
+        self.box3_text = ctk.CTkLabel(self.root, text="Quality:")
+        self.quality_dropdown = ctk.CTkComboBox(
+            self.root, 
+            state="readonly",
+            width=100,
+            values=["Lossless", *[str(i) for i in range(100, -1, -1)]]
         )
-        self.quality_dropdown.current(0)
+        self.quality_dropdown.set('Lossless')
         # TODO: find a way to add a "%" to the end of progressbar_text"
-        self.progressbar_text = tk.Label(self.root, textvariable=self.progress)
+        self.progressbar_text = ctk.CTkLabel(self.root, textvariable=self.progress)
 
-        self.progressbar = ttk.Progressbar(
+        self.progressbar = ctk.CTkProgressBar(
             self.root,
             variable=self.display_progress,
-            orient="horizontal",
-            length=300,
+            width=300,
             mode="determinate",
         )
 
         self.header.grid(row=0, column=2, columnspan=1)
-        self.box1_text.grid(row=3, column=1, sticky="w")
-        self.box2_text.grid(row=4, column=1, sticky="w")
-        self.box3_text.grid(row=5, column=1, sticky="w")
-        self.quality_dropdown.grid(row=5, column=2, sticky="w")
+        self.box1_text.grid(row=3, column=1, sticky="w", padx=(10, 0))
+        self.box2_text.grid(row=4, column=1, sticky="w", padx=(10, 0))
+        self.box3_text.grid(row=5, column=1, sticky="w", padx=(10, 0))
+        self.quality_dropdown.grid(row=5, column=1, sticky="w",padx=(60, 0))
         self.start_button.grid(row=6, column=2)
         self.progressbar_text.grid(row=7, column=2)
         self.progressbar.grid(row=8, column=2)
 
         self.fields = []
         for i in range(2):
-            path_field = tk.Entry(self.root, width=50)
-            path_field.grid(row=i + 3, column=2)
-            browse_button = tk.Button(
+            path_field = ctk.CTkEntry(self.root, width=250)
+            path_field.grid(row=i + 3, column=2 , pady=(5, 0))
+            browse_button = ctk.CTkButton(
                 self.root,
                 text="Browse...",
                 command=lambda field_num=i: self.browse(self.fields, field_num),
             )
-            browse_button.grid(row=i + 3, column=3)
+            browse_button.grid(row=i + 3, column=3, pady=(5, 0))
             self.fields.append(path_field)
 
-    def update_progressbar(self):
-        self.progressbar.update()
+    def update_progressbar(
+        self,
+        num_of_image_files,
+        num_of_non_image_files,
+        num_of_skipped_files,
+        file_list_length,
+    ):
+        num_of_processed_files = (
+            num_of_image_files + num_of_non_image_files + num_of_skipped_files
+        )
+        self.progress.set(f"{int((num_of_processed_files / file_list_length) * 100)}%")
+        self.display_progress.set(str(self.progress.get()).strip("%"))
 
     def post_conversion_dialogue(self, num_of_images, num_of_non_images):
         if num_of_images == 0:
-            tk.messagebox.showerror("Error", "No images were found in source folder.")
+            CTkMessagebox(
+                title="Error", 
+                message="No images were found in source folder.", 
+                icon="cancel"
+            )
             return False
         elif num_of_non_images > 0:
-            return tk.messagebox.askyesno(
-                "Copy non-images?",
-                f"""{num_of_images} files were successfully converted.
+            copy_non_images = CTkMessagebox(
+                title="Copy non-images?",
+                message=f"""{num_of_images} files were successfully converted.
 {num_of_non_images} files could not be converted. they are most likely not images.
 Copy non-images to destination folder?""",
+                icon="question",
+                option_1="Yes",
+                option_2="No"
             )
+            if copy_non_images.get() == "Yes":
+                return True
+            elif copy_non_images.get() == "No":
+                return False
+        
         else:
-            tk.messagebox.showinfo(
-                "Conversion complete!",
-                f"{num_of_images} files were successfully converted.",
+            CTkMessagebox(
+                title="Conversion complete!",
+                message=f"{num_of_images} files were successfully converted!",
+                icon="check",
+                option_1="Ok"
             )
             return False
+
+    # file overwrite dialogues
+    def confirm_overwrite_file(self, file_name):
+        overwrite = CTkMessagebox(
+            title="File already exists",
+            message=f'File "{file_name}.webp" already exists in the destination folder.\nWould you like to overwrite it?',
+            icon="warning",
+            option_1="Yes",
+            option_2="No"
+        )
+        if overwrite.get() == "Yes":
+            return True
+        elif overwrite.get() == "No":
+            return False
+
+
+    def confirm_overwrite_all(self):
+        overwrite_all = CTkMessagebox(
+            title="Overwrite all?",
+            message="Would you like to overwrite all files that already exist in the destination folder?",
+            icon="question",
+            option_1="Yes",
+            option_2="No"
+        )
+        if overwrite_all.get() == "Yes":
+            return True
+        elif overwrite_all.get() == "No":
+            return False
+
+    def confirm_are_you_sure(self):
+        are_you_sure = CTkMessagebox(
+            title="Are you sure?",
+            message="Are you sure you want to overwrite all files that already exist in the destination folder?\nThis action cannot be undone.",
+            icon="warning",
+            option_1="Yes",
+            option_2="No",
+            sound=True
+        )
+        if are_you_sure.get() == "Yes":
+            return True
+        elif are_you_sure.get() == "No":
+            return False
+    
+
+    def show_overwrite_dialogues(self, new_dst_path, are_you_sure):
+        # check if file already exists and prompt user to overwrite or skip
+        if os.path.isfile(new_dst_path.split(".")[0] + ".webp"):
+            if self.overwrite_all:
+                return True
+
+            else:
+                overwrite_file = self.confirm_overwrite_file(
+                    os.path.basename(
+                        new_dst_path.split(".")[0]
+                    )  # TODO: this somehow refreshes the window and advances the progress intvar?
+                )
+
+                if not overwrite_file:
+                    return False
+
+                while self.show_overwrite_all_dialogue and not are_you_sure:
+                    self.overwrite_all = self.confirm_overwrite_all()
+
+                    if self.overwrite_all:
+                        are_you_sure = self.confirm_are_you_sure()
+                        self.overwrite_all = are_you_sure
+
+                    else:
+                        self.overwrite_all = False
+                        self.show_overwrite_all_dialogue = False
+        return True  # file is to be overwritten
 
 
 if __name__ == "__main__":
