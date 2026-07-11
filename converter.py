0d472f63ef0e718444e8cc82f0579cd74221e0c7
shemishtamesh <shemishtamail@gmail.com>
2026-07-11 22:48:14 +0300

    fix(progress-bar): correctly calculate num of conversions resolves #33

--- converter.py
+++ converter.py
@@ -66,7 +66,6 @@ class Converter:  # TODO: find a better name so it doesn't conflict with the mod
             print(f"\x1b[2KConverting {full_dst_path.name} to {self.selected_format}...", end="\r")
             try:
                 image = Image.open(file)
-                num_of_converted_files += 1
             except UnidentifiedImageError:
                 num_of_failed_conversions += 1
                 non_image_list.append(file)
@@ -129,7 +128,6 @@ class Converter:  # TODO: find a better name so it doesn't conflict with the mod
                             pass
                         elif response.get() == "Downscale all":
                             self.downscale_all = True
-                            num_of_converted_files += 1
 
                     original_max = Image.MAX_IMAGE_PIXELS  # remove decomp bomb check for downscaling only
                     Image.MAX_IMAGE_PIXELS = None
@@ -155,6 +153,7 @@ class Converter:  # TODO: find a better name so it doesn't conflict with the mod
                     subsampling=0,
                 )
                 image.close()
+                num_of_converted_files += 1
                 image = None
 
             self.gui.update_progressbar(
